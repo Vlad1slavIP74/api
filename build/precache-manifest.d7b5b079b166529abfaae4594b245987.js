83f654201722b4a4e76c511e42ef21c1eb752b6c
Vlad <Vlad1slavIP74>
2019-04-29 19:40:38 +0300

    Last

--- build/precache-manifest.d7b5b079b166529abfaae4594b245987.js
+++ build/precache-manifest.d7b5b079b166529abfaae4594b245987.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "80d4a3d1e25c1a55bfe80e21e4f481d1",
+    "revision": "914f1d0d0227ac0ee6922f6f2d2d423c",
     "url": "/kassa/index.html"
   },
   {
-    "revision": "e31f59ac40696f7856f8",
+    "revision": "30af93a9176e8f6333c0",
     "url": "/kassa/static/css/main.795991f2.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/kassa/static/js/2.c8913e8d.chunk.js"
   },
   {
-    "revision": "e31f59ac40696f7856f8",
-    "url": "/kassa/static/js/main.8a1d36ed.chunk.js"
+    "revision": "30af93a9176e8f6333c0",
+    "url": "/kassa/static/js/main.76f7e8ca.chunk.js"
   },
   {
     "revision": "28185fc084eb8def9328",
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,6 +10,20 @@ class Main extends Component {
         <Link to='/success'>
           <Button bsStyle="primary" className="ButtonTable">Success</Button>
         </Link>
+
+
+          <form id="payment" name="payment" method="post" action="https://sci.interkassa.com/" enctype="utf-8">
+            <input type="hidden" name="ik_co_id" value="5cc701ae3b1eaf373c8b4579"></input>
+            <input type="hidden" name="ik_pm_no" value="ID_4233"></input>
+            <input type="hidden" name="ik_am" value="100.00" ></input>
+            <input type="hidden" name="ik_cur" value="UAH" ></input>
+            <input type="hidden" name="ik_desc" value="Event Description" ></input>
+            <input type="submit" value="Pay"></input>
+           
+          </form>
+
+
+
       </div>
     )
   }

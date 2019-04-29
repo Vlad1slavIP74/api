ff7e3142adb8c10279a6ab7d54f0eebd40543226
Vlad <Vlad1slavIP74>
2019-04-29 18:32:07 +0300

    Updates

---
+++ precache-manifest.6ab07f396ec842464cd5b5a85f48062e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a1f4084afae1da00fdf7cad8b9f3b3",
+    "url": "/kassa/index.html"
+  },
+  {
+    "revision": "235acb4deb1362856a0c",
+    "url": "/kassa/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "da296ac3eeea488d6729",
+    "url": "/kassa/static/js/2.c8913e8d.chunk.js"
+  },
+  {
+    "revision": "235acb4deb1362856a0c",
+    "url": "/kassa/static/js/main.d47fc37e.chunk.js"
+  },
+  {
+    "revision": "28185fc084eb8def9328",
+    "url": "/kassa/static/js/runtime~main.71082685.js"
+  }
+]);
